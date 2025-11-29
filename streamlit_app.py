d84dfb8c08c2e4b43433c2c4c775193d80a8fdda
Jesús Mira <50523575+jmiraal@users.noreply.github.com>
2025-11-29 13:12:21 +0100

    Comment out dataframe display and update API request
    Comment out dataframe display and stop calls. Update API request to use search_on value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -47,7 +47,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+
+        search_on =  pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SERARCH_ON'].iloc[0]
+        st.write('The search value for ',fruit_chosen,  ' is ', search_on,'.')
+      
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
